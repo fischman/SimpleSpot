8664da84e3b37e09e0ec33fbdc72fb45441af6c2
Ami Fischman <ami@fischman.org>
2026-01-10 06:18:50 +0000

    Add minimal server that only serves index.html

---
+++ server.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-Type', 'text/html')
+        self.end_headers()
+        with open('index.html', 'rb') as f:
+            self.wfile.write(f.read())
+    
+    def log_message(self, format, *args):
+        pass  # Suppress logging
+
+HTTPServer(('', 8000), Handler).serve_forever()
