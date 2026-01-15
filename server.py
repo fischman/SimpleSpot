af754b73df02a893e2ed974aff3566a8c5c30ebd
Ami Fischman <ami@fischman.org>
2026-01-15 02:31:13 +0000

    Inline PWA manifest into index.html
    - Use data URI for manifest instead of separate file
    - Remove manifest.json file
    - Update server.py to only serve logo.svg
    
    Co-authored-by: Shelley <shelley@exe.dev>

--- server.py
+++ server.py
@@ -4,7 +4,6 @@ from datetime import datetime
 
 # Allowed static files for PWA support
 STATIC_FILES = {
-    '/manifest.json': ('manifest.json', 'application/manifest+json'),
     '/logo.svg': ('logo.svg', 'image/svg+xml'),
 }
 
