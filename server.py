f2b3d2b787e1b56d9f8d105bfb6296836f2cbb29
Ami Fischman <ami@fischman.org>
2026-01-16 08:31:04 +0000

    Remove logging from server.py
    Co-authored-by: Shelley <shelley@exe.dev>

--- server.py
+++ server.py
@@ -1,19 +1,8 @@
 #!/usr/bin/env python3
 from http.server import HTTPServer, BaseHTTPRequestHandler
-from datetime import datetime
 
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
-        # Log request
-        with open('server.log', 'a') as f:
-            xff = self.headers.get('X-Forwarded-For')
-            if xff:
-                unique_ips = list(dict.fromkeys(ip.strip() for ip in xff.split(',')))
-                client_ip = ', '.join(unique_ips)
-            else:
-                client_ip = self.address_string()
-            f.write(f"{datetime.now().isoformat()} {client_ip} {self.requestline}\n")
-        
         self.send_response(200)
         self.send_header('Content-Type', 'text/html')
         self.end_headers()
